2490795bca571d184ed4588ee160548dc0f39a56
MonitThakkar <monitthakkar@gmail.com>
2022-04-05 15:29:52 +0530

    Revert "first commit"
    This reverts commit 96a3d43403577e2d9100ffad7f95a373a4b66c7f.

--- src/main/java/com/example/mysoc/repository/UserRepositoy.java
+++
@@ -1,7 +0,0 @@
-package com.example.mysoc.repository;
-
-import com.example.mysoc.entity.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepositoy extends MongoRepository<User,Long> {
-}
